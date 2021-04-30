dd67be0722d430b6a9628799333096bbf9a79d30
doityu <goldgolding67@gmail.com>
2021-04-30 16:19:37 +0900

    問題44回答

--- PE1...50/pe-44.py
+++ PE1...50/pe-44.py
@@ -6,20 +6,21 @@
 # 五角数のペア Pj と Pk について, 差と和が五角数になるものを考える.
 # 差を D = |Pk - Pj| と書く. 差 D の最小値を求めよ.
 
-num_li = []
-for i in range(1, 2000):
-    num_li.append( str(i * (3 * i - 1) // 2))
+# 回答方法自体は出来ていたがリストで回していたため非常に遅い
+# set にしたらすぐに回答が表示された
+# リスト等の探索スピードについて
+# https://qiita.com/cof/items/05f6ffc6d4e5b062aaa9
 
-penta_num = []
-for i in range(len(num_li)):
-    for j in range(i, len(num_li)):
-        plus = int(num_li[j]) + int(num_li[i])
-        minus = int(num_li[j]) - int(num_li[i])
-        if(plus > int(num_li[len(num_li)-1])):
-            break
-        if(str(plus) in num_li and str(minus) in num_li):
-            penta_num.append([i,j])
+# 答えを返す関数
+def findAns():
+    num_set = set(i * (3 * i - 1) // 2 for i in range(1, 9999))
 
-for i in range(len(penta_num)):
-    print(num_li[penta_num[i][0]], num_li[penta_num[i][1]])
-print(penta_num)
+    for i in num_set:
+        for j in num_set:
+            if(j > i):
+                plus = j + i
+                minus = j - i
+                if(plus in num_set and minus in num_set):
+                    return i - j
+
+print(findAns())
