03dbc1245a0ec988bffce23a7ccb17d9dabae206
doityu <goldgolding67@gmail.com>
2021-05-02 21:32:35 +0900

    次の関数を作成。ファイルから手札を取り出す関数。値が大きいカードを持つか判定する関数。勝利判定をする関数。役を判定する関数（途中）。

--- PE51...100/pe-54.py
+++ PE51...100/pe-54.py
@@ -6,18 +6,162 @@
 # ・各勝負で勝敗は必ず決まる
 
 # 1000回中プレイヤー1が勝つのは何回か? (訳注 : この問題に置いてA 2 3 4 5というストレートは考えなくてもよい)
+import sys
+from enum import Enum
+# カードの役の種類
+class CARD_RANK(Enum):
+    HIGH_CARD      = 0
+    ONE_PAIR       = 1
+    TWO_PAIRS      = 2
+    THREE_KIND     = 3
+    STRAIGHT       = 4
+    FLUSH          = 5
+    FULL_HOUSE     = 6
+    FOUR_KIND      = 7
+    STRAIGHT_FLUSH = 8
+    ROYAL_FLUSH    = 9
+# カードの値
+CARD_VALUES = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+# カードのスーツ
+CARD_SUIT = ['S', 'D', 'H', 'C']
 
-
-
-if __name__ == "__main__":
+# poker.txtからプレイヤー1と2の手札のリストを返す
+def preparation():
     f = open("./pe-54_poker.txt", "r")
     data = f.read()
     f.close
 
-    # カードの値
-    CARD_VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
-    
     data_list = data.split("\n")
     for i in range(len(data_list)):
         data_list[i] = data_list[i].split(" ")
+
+    p1_card_list = [[] for i in range(len(data_list))]
+    p2_card_list = [[] for i in range(len(data_list))]
+
+    for i in range(len(data_list)):
+        for j in range(5):
+            p1_card_list[i].append(data_list[i][j])
+            p2_card_list[i].append(data_list[i][j + 5])
+    return p1_card_list, p2_card_list
+
+# 一番値が大きいカードを比べる。同じ場合は次に大きいカードを比べる。
+# 以下同様に行い、プレイヤー1が値が大きいカードを持つ場合True
+def is_max_value(p1_card, p2_card):
+    p1_card_num = []
+    p2_card_num = []
+
+    # カードの値のみ摘出
+    for i in range(len(p1_card)):
+        p1_key = p1_card[i][:1]
+        p1_card_num.append(CARD_VALUES[p1_key])
+        p2_key = p2_card[i][:1]
+        p2_card_num.append(CARD_VALUES[p2_key])
+    # 降順にソート
+    p1_card_num = sorted(p1_card_num, reverse=True)
+    p2_card_num = sorted(p2_card_num, reverse=True)
+    # 値の比較を行い、同じ場合は次の値をチェックする
+    for i in range(len(p1_card_num)):
+        if(p1_card_num[i] > p2_card_num[i]):
+            return True
+        elif(p1_card_num[i] < p2_card_num[i]):
+            return False
+    else:
+        print("勝敗を決められません", p1_card, p2_card)
+        sys.exit(1)
+
+# お互いの手札の役が同じ場合に呼ばれる役の中身を比較する
+def get_compariosn_rank(p1_card, p2_card, rank):
+    return True
+
+# カードの役を返す
+def get_card_rank_and_value(card):
+    # カードを値と絵柄に分割
+    card_value = []
+    card_value_num = []
+    card_suit = []
+    for c in card:
+        card_value.append(c[:1])
+        key = c[:1]
+        card_value_num.append(CARD_VALUES[key])
+        card_suit.append(c[1:])
+    card_value_num = sorted(card_value_num, reverse=True)
+
+    # ストレートに並んでいるか
+    is_straight = False
+    for i in range(1, len(card_value_num)):
+        if(card_value_num[i-1] - card_value_num[i] != 1):
+            break
+    else:
+        is_straight = True
+
+    # カードの絵柄がすべて同じか
+    is_flash = False
+    for i in range(1, len(card_suit)):
+        if(card_suit[i] != card_suit[i-1]):
+            break
+    else:
+        is_flash = True
+
+    # 4つの役に該当するか判定
+    # ・ロイヤルストレートフラッシュ 
+    # ・ストレートフラッシュ 
+    # ・ストレート 
+    # ・フラッシュ 
+    if(is_straight and is_flash):
+        if(card_value[0] == CARD_VALUES['A']):
+            # ロイヤルストレートフラッシュ
+            return CARD_RANK.ROYAL_FLUSH
+        else:
+            # ストレートフラッシュ
+            return CARD_RANK.STRAIGHT_FLUSH
+    elif(is_straight):
+        # ストレート
+        return CARD_RANK.STRAIGHT
+    elif(is_flash):
+        # フラッシュ
+        return CARD_RANK.FLUSH
+
+
+
+    # 役がない場合
+    return CARD_RANK.HIGH_CARD
+
+# プレイヤー１の手札がプレイヤー2の手札に勝っているか判定する
+def is_win_player1(p1_card, p2_card):
+    p1_rank = get_card_rank_and_value(p1_card)
+    p2_rank = get_card_rank_and_value(p2_card)
+    # カードの役が大きければ勝ち
+    if(p1_rank.value > p2_rank.value):
+        return True
+    elif(p1_rank.value == p2_rank.value):
+        # カードの役が同じ場合、役を構成する中で値が最も大きい方が勝ち
+        p1_value, p2_value = compariosn_rank(p1_card, p2_card, p1_rank)
+        if(p1_value > p2_value):
+            return True
+        elif(p1_value == p2_value):
+            # 上記の値が同じ場合、一番値が大きいカード方が勝ち
+            return is_max_value(p1_card, p2_card)
+        else:
+            return False
+    else:
+        return False
+
+if __name__ == "__main__":
+    p1_card_list, p2_card_list = preparation()
+    print(p1_card_list[0], p2_card_list[0])
+
+    p1_win = 0
+    # for i in range(len(p1_card_list)):
+    #     if(is_win_player1(p1_card_list[i], p2_card_list[i])):
+    #         p1_win += 1
+    # print(p1_card_list[0][0][:1], p1_card_list[0][0][1:])
+    # card_rank = get_card_rank_and_value(1)
+    # print(card_rank)
+
+    # print(sorted(['1','Q','A','T', '4']))
+    royal = ['TC', 'JC', 'QC', 'KC', 'AC']
+    sample = ['AC', 'QC', 'TC', 'KS', 'JS']
+
+    print(get_card_rank_and_value(sample))
+
 
